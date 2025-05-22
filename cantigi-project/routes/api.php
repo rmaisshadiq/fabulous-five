6a49499f644533eb5d56d68c518734fcda0b198a
Rafi Maisshadiq <74971414+rmaisshadiq@users.noreply.github.com>
2025-05-22 08:37:51 +0700

    chore: add endpoint api to get all vehicles

--- cantigi-project/routes/api.php
+++ cantigi-project/routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\VehicleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+// Route::get('/vehicles', [VehicleController::class, 'index']);
+
+Route::group(['prefix' => 'vehicles'], function() {
+    Route::get('/all', [VehicleController::class, 'index']);
+});
