ee4e7fa3e7617463ef38198f950cb61f02082d2b
Rafi Maisshadiq <74971414+rmaisshadiq@users.noreply.github.com>
2025-05-22 15:25:05 +0700

    chore: added protected fillables to each column

--- cantigi-project/app/Models/Maintenance.php
+++ cantigi-project/app/Models/Maintenance.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Maintenance extends Model
 {
     //
+    use HasFactory;
+
+    protected $fillable = [
+        'vehicle_id',
+        'maintenance_date',
+        'description',
+        'cost'
+    ];
 }
--- cantigi-project/app/Models/ReturnLog.php
+++ cantigi-project/app/Models/ReturnLog.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ReturnLog extends Model
 {
     //
+    use HasFactory;
+
+    protected $fillable = [
+        'return_date',
+        'return_time'
+    ];
 }
