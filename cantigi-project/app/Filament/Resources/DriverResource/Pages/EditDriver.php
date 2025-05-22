e6729834cad006757afa463dc7d0b756166f37c4
Delonicligia <delonicligia@gmail.com>
2025-05-22 16:56:31 +0700

    feat: membuat fitur untuk driver

---
+++ cantigi-project/app/Filament/Resources/DriverResource/Pages/EditDriver.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\DriverResource\Pages;
+
+use App\Filament\Resources\DriverResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditDriver extends EditRecord
+{
+    protected static string $resource = DriverResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
