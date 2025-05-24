4f36385d619ba07dadcf53b86b608423a7a0cb34
Rafi Maisshadiq <74971414+rmaisshadiq@users.noreply.github.com>
2025-05-24 11:07:13 +0700

    fix: resolves conflict

--- cantigi-project/routes/web.php
+++ cantigi-project/routes/web.php
@@ -2,10 +2,16 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Vehicle;
 
 Route::get('/', function () {
-    return view('LandingPage/MainContent');
-});
+    return view('LandingPage.homePage');
+})->name('home');
+
+Route::get('/car', function () {
+    $vehicles = Vehicle::where('status', 'active')->get(); // ambil kendaraan yang aktif
+    return view('kendaraan.car.mainPageMobil', compact('vehicles'));
+})->name('kendaraan');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
