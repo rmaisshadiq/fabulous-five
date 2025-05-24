9d54b8fe99d4e1f0913841e2841e3c240446666e
ZakyAzda <zakiazda44@gmail.com>
2025-05-24 11:32:24 +0700

    feat menambahkan fitur artikel

--- cantigi-project/app/Models/Article.php
+++ cantigi-project/app/Models/Article.php
@@ -18,6 +18,6 @@ class Article extends Model
     ];
 
     public function employees() {
-        return $this->belongsTo(Employee::class);
+        return $this->belongsTo(Employee::class, 'author_id');
     }
 }
