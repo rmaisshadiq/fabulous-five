afecd2e9b16e41a4ce315aaf87fc985b039a696e
ZakyAzda <zakiazda44@gmail.com>
2025-06-17 21:29:10 +0700

    feat: membuat feedback dan payment resource pada panel admin

---
+++ cantigi-project/app/Filament/Resources/FeedbackResource/Pages/ListFeedback.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Filament\Resources\FeedbackResource\Pages;
+
+use App\Filament\Resources\FeedbackResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListFeedback extends ListRecords
+{
+    protected static string $resource = FeedbackResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return []; // Ini menghilangkan tombol 'New feedback'
+    }
+}
