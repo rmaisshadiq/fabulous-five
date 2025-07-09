58bf443ac465be31f1b4919e689ca1f9257bbfc9
Rafi Maisshadiq <74971414+rmaisshadiq@users.noreply.github.com>
2025-07-09 18:07:04 +0700

    chore: add option export to excel in customer

--- cantigi-project/app/Filament/Resources/CustomerResource/Pages/ListCustomers.php
+++ cantigi-project/app/Filament/Resources/CustomerResource/Pages/ListCustomers.php
@@ -4,8 +4,12 @@ namespace App\Filament\Resources\CustomerResource\Pages;
 
 use App\Filament\Imports\CustomerImporter;
 use App\Filament\Resources\CustomerResource;
+use App\Models\Customer;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use pxlrbt\FilamentExcel\Columns\Column;
+use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
+use pxlrbt\FilamentExcel\Exports\ExcelExport;
 
 class ListCustomers extends ListRecords
 {
@@ -16,6 +20,18 @@ class ListCustomers extends ListRecords
         return [
             Actions\CreateAction::make()
                 ->label('Buat Pelanggan Baru'),
+            ExportAction::make()
+                ->label("Export ke Excel")
+                ->exports([
+                    ExcelExport::make()->withColumns([
+                        Column::make('user.name')
+                            ->heading("Nama Pelanggan")
+                            ->getStateUsing(fn($record) => $record->user?->name ?? $record->name),
+                        Column::make('phone_number')
+                            ->heading("Nomor HP")
+                            ->getStateUsing(fn($record) => $record->phone_number ?? "Belum tersedia"),
+                    ])
+                ]),
             Actions\ImportAction::make()
                 ->importer(CustomerImporter::class)
                 ->label('Import dari Excel')
