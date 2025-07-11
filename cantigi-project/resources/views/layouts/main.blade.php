d6eb63957d7c3f684d03b1571c67c77e511a80a4
Rafi Maisshadiq <74971414+rmaisshadiq@users.noreply.github.com>
2025-07-11 17:41:41 +0700

    chore: add crsf_token

--- cantigi-project/resources/views/layouts/main.blade.php
+++ cantigi-project/resources/views/layouts/main.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Cantigi Tours</title>
     
     {{-- Tailwind dari CDN masih bisa dipakai, tapi disarankan pakai Vite jika kamu compile Tailwind --}}
